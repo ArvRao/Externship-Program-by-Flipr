5507a4e623107c1e00fab1861ddc634a1ec08590
arvrao <arvindrao100@gmail.com>
2022-01-28 23:36:16 +0530

    updated comment and post models

---
+++ src/models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    imageUrl: { type: String,
+    default: null
+ },
+    cloudinaryId: { type: String,
+     default: null
+ },
+    caption: { type: String,
+     trim: true },
+    creator: { type: mongoose.Schema.Types.ObjectId,
+     ref: "User" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId,
+         ref: "User" }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
